c90da9336125cf8597cf8f92e4e470104329415a
sudarshan <vijays@fysik.dtu.dk>
2021-08-16 13:26:00 +0200

    Adjusting versions on setup.json

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
-from setuptools import setup, find_packages
-import json
+# -*- coding: utf-8 -*-
+"""Define the setup for the `aiida-ase` plugin."""
+try:
+    import fastentrypoints  # pylint: disable=unused-import
+except ImportError:
+    # This should only occur when building the package, i.e. for `python setup.py sdist/bdist_wheel`
+    pass
+
+
+def setup_package():
+    """Install the `aiida-ase` package."""
+    import json
+    from setuptools import setup, find_packages
+
+    filename_setup_json = 'setup.json'
+    filename_description = 'README.md'
+
+    with open(filename_setup_json, 'r') as handle:
+        setup_json = json.load(handle)
+
+    with open(filename_description, 'r') as handle:
+        description = handle.read()
+
+    setup(
+        include_package_data=True,
+        packages=find_packages(),
+        long_description=description,
+        long_description_content_type='text/markdown',
+        **setup_json
+    )
+
 
 if __name__ == '__main__':
-    setup(
-        name='aiida-catmap',
-        packages=['aiida_catmap'], 
-        entry_points={
-            'aiida.calculations':['catmap = aiida_catmap.calculations:CatMAPCalculation'], 
-            'aiida.parsers':['catmap = aiida_catmap.parsers:CatMAPParser'],
-            }
-        )
+    setup_package()
